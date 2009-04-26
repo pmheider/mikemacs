fd8ba2af90e2d44cf56d81297617219d9bdbf3a6
Mike Prentice <mjp44@buffalo.edu>
2009-04-26 03:08:28 -0400

    Added fixssh function to mikefun.el
    fixssh reads from ~/bin/fixssh to set X forwarding environment
    variables.  Useful when running emacs in GNU screen but still
    need to be able to start X processes.

--- mikefun.el
+++ mikefun.el
@@ -19,4 +19,19 @@
         ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
         (t (self-insert-command (or arg 1)))))
 
+(defun fixssh ()
+  "Run fixssh script for use in GNU screen with X forwarding"
+  (interactive)
+  (save-excursion
+    (let ((buffer (find-file-noselect "~/bin/fixssh")))
+      (set-buffer buffer)
+      (setq buffer-read-only t)
+      (goto-char (point-min))
+      (while (re-search-forward
+              "\\([A-Z_][A-Z0-9_]*\\) *= *\"\\([^\"]*\\)\"" nil t)
+        (let ((key (match-string 1))
+              (val (match-string 2)))
+          (setenv key val)))
+      (kill-buffer buffer))))
+
 (provide 'mikefun)
