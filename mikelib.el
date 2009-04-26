1a68807053a131052189865da54e44ee31cb959a
Mike Prentice <mjp44@buffalo.edu>
2009-04-26 11:27:24 -0400

    Move machine-specific setq back to .emacs

--- mikelib.el
+++ mikelib.el
@@ -1,13 +1,6 @@
 ;; Library-specific initializations
 
 (when (featurep 'slime)
-  (setq inferior-lisp-program "/Applications/AllegroCL/mlisp")
-  (setq slime-lisp-implementations
-        '((mlisp ("/Applications/AllegroCL/mlisp"))
-          (alisp ("/Applications/AllegroCL/alisp"))
-          (sbcl ("/opt/local/bin/sbcl"))
-          (ccl ("/Users/mprentice/bin/ccl64"))
-          (clisp ("/opt/local/bin/clisp"))))
   (slime-setup))
 (when (featurep 'ido) (ido-mode t))
 (when (featurep 'yasnippet)
