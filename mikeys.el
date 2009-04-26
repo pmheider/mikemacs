c8f2350c925db2f91818f45e89055424c2ef07b7
Mike Prentice <mjp44@buffalo.edu>
2009-04-26 15:41:13 -0400

    Undo lisp comment keybindings (M-; instead)

--- mikeys.el
+++ mikeys.el
@@ -9,8 +9,4 @@
 ;; text-mode keymappings
 (define-key text-mode-map (kbd "TAB") 'self-insert-command);
 
-;; Lisp mode keymappings
-(define-key lisp-mode-map (kbd "C-c ;") 'comment-region)
-(define-key emacs-lisp-mode-map (kbd "C-c ;") 'comment-region)
-
 (provide 'mikeys)
