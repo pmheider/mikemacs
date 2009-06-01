bf2e85ae2950eaef0ba42c4c7e48fca3c0e8f6dc
Mike Prentice <mjp44@buffalo.edu>
2009-06-01 13:30:45 -0400

    ido and git-emacs not working together
    git-emacs expects a function ido-completing-read that is not defined.  I have
    set git--completing-read to use completing-read function instead.  This can
    also be set with a custom variable, git--use-ido, but that has to be set prior
    to loading git-emacs (I think, I am not that familiar with custom).

--- mikelib.el
+++ mikelib.el
@@ -11,5 +11,7 @@
   (yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets"))
 (setq py-python-command-args '( "-colors" "NoColor"))
 (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
+(when (featurep 'git-emacs)
+  (setq git--completing-read #'completing-read)) ; ido not working, why?
 
 (provide 'mikelib)
