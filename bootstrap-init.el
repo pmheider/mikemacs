e639cb5eb2460f380fe90d5e0318f3e5d8cdb6b7
Mike Prentice <mjp44@buffalo.edu>
2009-06-04 23:58:28 -0400

    allow versioning before "before.el"

--- bootstrap-init.el
+++ bootstrap-init.el
@@ -1,4 +1,5 @@
 ;;; From http://www.io.com/~jimm/emacs_tips.html
+;;; Modified by Mike Prentice
 
 (defun bootstrap-file (machine file-name)
   (concat *my-emacs-lib-dir* "bootstrap/" machine "/" file-name))
@@ -9,6 +10,7 @@
 	(load f))))
 
 (defun bootstrap-init (machine)
+  (load (concat *my-emacs-lib-dir* "emacs-variants")) ; allows versioning
   (load-init-if-exists machine "before")
   (load (concat *my-emacs-lib-dir* "mikemacs"))
   (load-init-if-exists machine "after")
