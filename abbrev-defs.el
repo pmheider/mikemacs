63aa8718d55c0f9617670f39841957d69173b847
Michael J Prentice <mjp44@buffalo.edu>
2009-06-09 11:17:09 -0400

    fixed paths on cse-linux

---
+++ abbrev-defs.el
@@ -0,0 +1,68 @@
+(define-abbrev-table 'eshell-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'inferior-sml-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'comint-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'sml-yacc-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'sml-lex-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'sml-cm-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'sml-mode-abbrev-table '(
+    ("fun" "" sml-form-fun 0)
+    ("if" "" sml-form-if 0)
+    ("local" "" sml-form-local 0)
+    ("datatype" "" sml-form-datatype 0)
+    ("fn" "" sml-form-fn 0)
+    ("structure" "" sml-form-structure 0)
+    ("val" "" sml-form-val 0)
+    ("struct" "" sml-form-struct 0)
+    ("functor" "" sml-form-functor 0)
+    ("abstype" "" sml-form-abstype 0)
+    ("let" "" sml-form-let 0)
+    ("signature" "" sml-form-signature 0)
+    ("sig" "" sml-form-sig 0)
+    ("case" "" sml-form-case 0)
+    ))
+
+(define-abbrev-table 'log-edit-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'vc-log-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'vc-annotate-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'vc-dired-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'occur-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'text-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'lisp-interaction-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'emacs-lisp-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'lisp-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'fundamental-mode-abbrev-table '(
+    ))
+
+(define-abbrev-table 'global-abbrev-table '(
+    ))
+
