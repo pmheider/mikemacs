e833c3ece6d8e1faa05b735f41c0489e605befdf
Michael J Prentice <mjp44@nickelback.cse.buffalo.edu>
2009-06-17 17:53:46 -0400

    Added C-M-y clipboard yank and M-s save for CSE systems

--- bootstrap/cse-linux/after.el
+++ bootstrap/cse-linux/after.el
@@ -5,3 +5,11 @@
 (if (eq (emacs-variant) 'xemacs)
     (require 'tex-site))
 
+;; Fix broken yank to yank from clipboard
+;; Yank from Emacs: C-y
+;; Yank from system clipboard: C-M-y
+(global-set-key "\C-\M-y" 'clipboard-yank)
+
+;; Shortcut since nothing seems to use it
+;; Save buffer: M-s
+(global-set-key "\M-s" 'save-buffer)
