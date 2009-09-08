c1b98548a84a3858d6f3577ec29d5da527849196
Michael J Prentice <mjp44@timberlake.cse.buffalo.edu>
2009-09-08 10:37:14 -0400

    Changed comint hook to emacs-only

--- mikemacs.el
+++ mikemacs.el
@@ -102,7 +102,8 @@
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
 ;; Turn off auto-filling in shells
-(add-hook 'comint-mode-hook (lambda () (turn-off-auto-fill)))
+(when (eq (emacs-variant) 'emacs)
+  (add-hook 'comint-mode-hook (lambda () (turn-off-auto-fill))))
 
 (add-hook 'graphviz-dot-mode-hook
           (lambda () (setq graphviz-dot-auto-indent-on-semi nil)))
